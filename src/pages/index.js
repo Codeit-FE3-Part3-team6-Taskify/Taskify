4d9cf378e2bd8321f773ef80036e6b5b6373bbce
nyaknya <nyaknya@naver.com>
2024-03-12 19:36:38 +0900

    🔥 remove: 테스트용 더미 파일 삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,3 @@
-import CtaDefault from '@/components/CtaDefault/CtaDefault';
-
 export default function Home() {
-  return (
-    <div className="max-w-xl mt-32 mx-auto">
-      <CtaDefault type="button" color="white" size="">
-        테스트
-      </CtaDefault>
-    </div>
-  );
+  return <>6팀 화이팅~</>;
 }
