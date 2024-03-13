aa5fcd268b3822cce899f539b2959e995365289f
노진석 <shwlstjr08@naver.com>
2024-03-13 14:16:08 +0900

    ✨ feat : 로그인, 회원가입 input  (#11)
    * ✨ feat : EmailInput 완성
    
    * ✨ feat : PasswordInput 완성
    
    * ♻️ refactor : 닉네임,이메일 input 공통으로 사용
    
    * 💩 feat : SignInPage 테스트 추가 및 기타 수정
    
    * 💩 chore : alt추가
    
    * 💄 chore : signinPage 디자인 수정
    
    * 💄 chore : SignIn margin-top 반응형으로 추가
    
    * ✨ feat : axiosInstance 생성
    
    * 💩 chore : 로그인 테스트
    
    * 💩 chore : 로그인 테스트 완료
    
    * 💩 chore : axios interceptor axiosInstance로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        black_000000: { DEFAULT: '#000000' },
+        black_171717: { DEFAULT: '#171717' },
+        black_333236: { DEFAULT: '#333236', 1: '#2순위컬러' },
+        black_4B4B4B: { DEFAULT: '#4B4B4B' },
+        gray_787486: { DEFAULT: '#787486' },
+        gray_9FA6B2: { DEFAULT: '#9FA6B2' },
+        gray_D9D9D9: { DEFAULT: '#D9D9D9' },
+        gray_EEEEEE: { DEFAULT: 'EEEEEE' },
+        gray_FAFAFA: { DEFAULT: '#FAFAFA' },
+        white_FFFFFF: { DEFAULT: '#FFFFFF' },
+        violet_5534DA: { DEFAULT: '#5534DA' },
+        'violet_8%': { DEFAULT: '#F1EFFD' },
+        red_D6173A: { DEFAULT: '#D6173A' },
+        green_7AC555: { DEFAULT: '#7AC555' },
+        purple_760DDE: { DEFAULT: '#760DDE' },
+        orange_FFA500: { DEFAULT: '#FFA500' },
+        blue_76A6EA: { DEFAULT: '#76A5EA' },
+        blue_5534da: { DEFAULT: '#5534da' },
+        pink_E876EA: { DEFAULT: '#E876EA' },
+      },
+      screens: {
+        md: '768px',
+        lg: '1024px',
       },
     },
   },
