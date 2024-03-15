93092c829cff6831953ff04d3ec06e106a0cdc64
심은주 <nyaknya@naver.com>
2024-03-15 18:17:27 +0900

    💄 style: 버튼 round 변경, 알럿 모달 관련 스타일 변경

--- src/components/Modal/AlertModal/AlertModal.jsx
+++ src/components/Modal/AlertModal/AlertModal.jsx
@@ -4,9 +4,9 @@ import CtaDefault from '@/components/Buttons/CtaDefault/CtaDefault';
 export default function AlertModal({ onClose, text }) {
   return (
     <Modal onClose={onClose}>
-      <div className="md:w-[540px] md:h-[250px] sm:w-full sm:h-[220px] sm:px-6 py-7 relative flex justify-center items-center">
-        <h2 className="text-lg pb-3">{text}</h2>
-        <div className="absolute right-7 bottom-7">
+      <div className="md:w-[540px] md:h-[250px] sm:w-[80vw] sm:h-[220px] py-7 relative flex justify-center items-center">
+        <h2 className="text-lg pb-5">{text}</h2>
+        <div className="absolute md:right-7 md:bottom-7 md:translate-x-0 sm:right-2/4 sm:translate-x-2/4">
           <CtaDefault onClick={onClose}>확인</CtaDefault>
         </div>
       </div>
