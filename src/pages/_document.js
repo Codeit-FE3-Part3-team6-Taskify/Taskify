7ceb7cd83132761198cdfaae6af5ae103e7dc60a
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-03-15 17:44:27 +0900

    💩 fix : 오타수정

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,8 +7,7 @@ export default function Document() {
       <link
         rel="stylesheet"
         as="style"
-        // eslint-disable-next-line react/no-unknown-property
-        crossorigin
+        crossOrigin
         href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
       />
 
