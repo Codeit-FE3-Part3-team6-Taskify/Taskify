97b4e79ef81eaa9975959911f7dc167c4724fee3
노진석 <shwlstjr08@naver.com>
2024-03-15 11:56:04 +0900

    ✨ Feat : 로그인/회원가입페이지 완성 (#22)
    * ✨ feat : EmailInput 완성
    
    * ✨ feat : PasswordInput 완성
    
    * ♻️ refactor : 닉네임,이메일 input 공통으로 사용
    
    * 💩 feat : SignInPage 테스트 추가 및 기타 수정
    
    * 💩 chore : alt추가
    
    * 💄 chore : signinPage 디자인 수정
    
    * 💄 chore : SignIn margin-top 반응형으로 추가
    
    * ✨ feat : axiosInstance 생성
    
    * 💩 chore : 로그인 테스트
    
    * 💩 chore : 로그인 테스트 완료
    
    * 💩 chore : axios interceptor axiosInstance로 수정
    
    * ✨ feat : 회원가입 기본 기능 구현(api x)
    
    * 🐛 chore : } 제거
    
    * 💄 chore : tailwind base에 맞게 디자인 수정
    
    * ✨ feat : 회원가입 api연동
    
    * 💩 feat : SignLink, SignLogo 컴포넌트화
    
    * ✨ feat : 로그인 시 redux에 데이터 담기
    
    * 💄 chore : 버튼 수정 및 기타 스타일 수정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Provider } from 'react-redux';
+import store from '@/features/store';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
