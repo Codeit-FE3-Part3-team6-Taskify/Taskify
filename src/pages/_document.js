1748a81786229202e0fd2a40758570d9c8aead47
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-03-15 18:01:21 +0900

    💩 fix : 서버 콘솔 경고 하나 해결

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
       <link
         rel="stylesheet"
         as="style"
-        crossOrigin
+        crossOrigin="true"
         href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
       />
 
