4d763c8359282eefa9bf916bd1992fffd8579ccc
심은주 <nyaknya@naver.com>
2024-03-16 18:38:51 +0900

    💄 style: 알럿 반응형 버튼 위치 수정

--- src/components/Modal/AlertModal/AlertModal.jsx
+++ src/components/Modal/AlertModal/AlertModal.jsx
@@ -6,7 +6,7 @@ export default function AlertModal({ onClose, text }) {
     <Modal onClose={onClose}>
       <div className="md:w-[540px] md:h-[250px] sm:w-[80vw] sm:h-[220px] py-7 relative flex justify-center items-center">
         <h2 className="text-lg pb-5">{text}</h2>
-        <div className="absolute md:right-7 md:bottom-7 md:translate-x-0 sm:right-2/4 sm:translate-x-2/4">
+        <div className="absolute md:right-7 md:translate-x-0 sm:right-2/4 sm:translate-x-2/4 sm:bottom-7">
           <CtaDefault onClick={onClose}>확인</CtaDefault>
         </div>
       </div>
