edf7bd5ef37e96d0faf93fbcde5b0bc36d5aa1f1
SongSang <941227_@naver.com>
2024-03-18 03:36:47 +0900

    ✨ feat:새로운 대시보드 추가 버튼 추가

--- src/components/MydashboardPage/DashboardList.jsx
+++ src/components/MydashboardPage/DashboardList.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
-import { NoMailIcon } from '../../../public/images';
+import { AddButtonEmpty, NoMailIcon } from '../../../public/images';
 import DashboardListItem from '../Buttons/DashboardListItem/DashboardListItem';
 import PaginationButton from '../Buttons/PaginationButton/PaginationButton';
 
-// TODO(송상훈) : 비어있음 div 이미지 추가
+// Todo(송상훈): 모달 컴포넌트 연동시기에 버튼 컴포넌트에 연동 해야함
 export default function DashboardList({
   dashboards,
   totalCount,
@@ -16,7 +16,16 @@ export default function DashboardList({
 
   return (
     <section className="bg-white max-w-[1022px] h-auto rounded-[8px] shadow-sm py-[24px] px-[16px]">
-      <h2 className="text-xl font-bold mb-5">대시보드 목록</h2>
+      <div className="flex justify-between mb-5">
+        <h2 className="text-xl font-bold">대시보드 목록</h2>
+        <button
+          className="flex items-center gap-2 h-[28px] md:border p-4 rounded"
+          type="button"
+        >
+          <span className="hidden text-sm  md:block">새로운 대시보드</span>
+          <Image width={25} height={25} src={AddButtonEmpty} alt="더하기버튼" />
+        </button>
+      </div>
 
       {isEmpty ? (
         <div className="h-[200px] flex flex-col justify-center items-center">
@@ -30,7 +39,7 @@ export default function DashboardList({
         </div>
       ) : (
         <>
-          <div className="flex flex-col h-[388px] gap-y-2 md:grid md:grid-cols-2 gap-[10px] lg:grid-cols-3 md:h- lg:h-[148px]">
+          <div className="flex flex-col h-[388px] gap-y-2 md:grid md:grid-cols-2 gap-[10px] lg:grid-cols-3 md:h-[220px] lg:h-[148px]">
             {dashboards.map((dashboard) => (
               <DashboardListItem
                 key={dashboard.id}
