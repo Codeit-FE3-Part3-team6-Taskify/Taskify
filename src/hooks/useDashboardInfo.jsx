bdc4d05771c78c2e4ff00d9516a3712fe04231c9
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-03-23 19:02:35 +0900

    🐛 fix : 추방 된 유저가 진입 시 이동 추가 및 오류 수정

--- src/hooks/useDashboardInfo.jsx
+++ src/hooks/useDashboardInfo.jsx
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { axiosGet } from '@/features/axios';
 import { setMembers } from '@/features/memberSlice';
@@ -10,20 +11,31 @@ export default function useDashboardInfo(dashboardId) {
   const memberList = useSelector((state) => state.memberList.members);
   const columns = useSelector((state) => state.columnList);
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const getDashboardInfo = async () => {
     const res = await axiosGet(`/dashboards/${dashboardId}`);
+    if (res.status) {
+      router.push('/mydashboard');
+      return;
+    }
     dispatch(setDashboardInfo({ data: res }));
   };
   const getMemberList = async () => {
     const res = await axiosGet(
       `members?page=1&size=20&dashboardId=${dashboardId}`,
     );
+    if (res.status) {
+      return;
+    }
     dispatch(setMembers({ members: res.members, totalCount: res.totalCount }));
   };
   const getColumns = async () => {
     dispatch(reset());
     const res = await axiosGet(`columns?dashboardId=${dashboardId}`);
+    if (res.status) {
+      return;
+    }
     dispatch(setColumn({ data: res.data }));
   };
   useEffect(() => {
--- src/pages/dashboard/[dashboardId].jsx
+++ src/pages/dashboard/[dashboardId].jsx
@@ -40,6 +40,9 @@ export default function DashboardPage({ dashboardId }) {
   const settingClick = () => {
     router.push(`/dashboard/${dashboardId}/edit`);
   };
+  if (!dashboardInfo || !memberList || !columns) {
+    return null;
+  }
 
   return (
     <div className="flex w-screen">
