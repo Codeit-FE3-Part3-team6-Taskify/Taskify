1b86f975637c2edbf5923b277febd54e46c581bd
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-03-29 13:51:01 +0900

    Revert "🐛 : 버그픽스 및 css 수정 (#81)" (#82)
    This reverts commit f1bc0acf39b341168dae65a2b97d5d259962db07.

--- src/utils/getRandomColor.js
+++
@@ -1,7 +0,0 @@
-import { BACKGROUND_COLORS } from '@/constants/colors';
-
-export default function getRandomColor() {
-  return BACKGROUND_COLORS[
-    Math.floor(Math.random() * BACKGROUND_COLORS.length)
-  ];
-}
